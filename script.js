95b103dc84c6f2d4823a61246b6ecf1ebaf8783b
jesperanderson <150336326+jesperanderson@users.noreply.github.com>
2024-09-17 13:36:23 +0200

    basics: html, css, script

---
+++ script.js
@@ -0,0 +1,48 @@
+let homeDOM = document.getElementsByTagName('a')[0];
+let contactDOM = document.getElementsByTagName('a')[1];
+let aboutDOM = document.getElementsByTagName('a')[2];
+
+let contentDOM = document.getElementById('content'); 
+
+
+fetchPage('home.html');
+
+
+homeDOM.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    history.pushState({ page: 'home' }, "", "/home");
+    fetchPage('home.html');
+});
+
+aboutDOM.addEventListener('click', (event) => {
+    event.preventDefault();
+    history.pushState({ page: 'about' }, "", "/about");
+    fetchPage('about.html');
+});
+
+contactDOM.addEventListener('click', (event) => {
+    event.preventDefault();
+    history.pushState({ page: 'contact' }, "", "/contact");
+    fetchPage('contact.html');
+});
+
+
+function fetchPage(filename) {
+    if (typeof filename !== "string") return;
+
+    fetch(filename)
+        .then((result) => result.text())
+        .then((data) => {
+            contentDOM.innerHTML = data;
+        })
+        .catch((error) => {
+            contentDOM.innerHTML = '<p>Kunde inte ladda innehållet.</p>';
+        });
+}
+
+
+window.addEventListener('popstate', (event) => {
+    let page = event.state ? event.state.page : 'home';
+    let filename = `${page}.html`;
+    fetchPage(filename);
+});
